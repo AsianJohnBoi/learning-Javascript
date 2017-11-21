c4f334916f8e31a5e363cdc33e1d322c986a5b7c
John Santias <32500331+AsianJohnBoi@users.noreply.github.com>
2017-11-21 16:22:48 +1000

    Create Arrays_1a.js

---
+++ Arrays_5/Arrays_1a.js
@@ -0,0 +1,11 @@
+let newYearsResolutions = ["Get fat", "Get fit", "Get a GPA 7", "Get good", "Learn more"];
+console.log(newYearsResolutions);
+
+newYearsResolutions[1] = 'Learn a new language';
+
+let listItem = newYearsResolutions[0];
+console.log(listItem);
+console.log(newYearsResolutions[2]);
+console.log(newYearsResolutions.Length());
+newYearsResolutions.pop(4);
+console.log(newYearsResolutions);
