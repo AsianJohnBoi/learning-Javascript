591de1cce75bdb317bb80e4afaa91e6b241715a3
John Santias <32500331+AsianJohnBoi@users.noreply.github.com>
2017-11-21 18:17:27 +1000

    Create Loops_1a.js

---
+++ Loops_6/Loops_1a.js
@@ -0,0 +1,5 @@
+let vacationSpots = ["Singapore", "Madrid", "Melbourne"];
+
+for (let vacationSpotIndex = 0; vacationSpotIndex < vacationSpots.length; vacationSpotIndex++){
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}	
